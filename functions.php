40e7ecf336746f2775bbeea669b97a17cc5895be
Ryan Rudolph <rudolphdesigns@gmail.com>
2016-03-24 09:26:31 -0400

    updated font awesome to 4.5.0

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function prometheus_google_fonts() {
 add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
 function enqueue_font_awesome() {
 
-	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
 
 }
 

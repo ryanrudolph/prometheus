30b57a54886b29148941a8fba3d298420268ce53
Ryan Rudolph <rudolphdesigns@gmail.com>
2016-05-25 09:30:00 -0400

    latest font awesome

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ function genesis_sample_enqueue_scripts_styles() {
 
   wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
   wp_enqueue_style( 'dashicons' );
-  wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css' );
+  wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
 
   wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
   $output = array(

d778f33ccfa73de3fa911429b3257ff8e70069e2
Ryan Rudolph <rudolphdesigns@gmail.com>
2016-06-02 10:05:57 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -14,7 +14,7 @@
 // Force full width 
 add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' ); 
 
-// Remove Page Title (HTML5)
+// Remove page title
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
 genesis();
